78e2f91dabafea6343343fac80267c10ed875e76
komaly <kyaseen@uci.edu>
2018-10-16 13:02:00 -0700

    Calculator - Rails

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,45 +1,96 @@
 var expression = ""
+var toAdd = ""
 
 function adding() {
-	expression += document.getElementById("display").value; 
+	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
+		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
+		s[s.length - 1] === "")
+	{
+		return;	
+	}
+
 	expression += " "
 	expression += document.getElementById("plus").value
-	window.alert(expression)
+	expression += " "
 }
 
 function subtracting() {
-	expression += document.getElementById("display").value; 
+	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
+		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
+		s[s.length - 1] === "")
+	{
+		return;	
+	}
+
 	expression += " "
 	expression += document.getElementById("minus").value
-	window.alert(expression)
+	expression += " "
 }
 
 function multiplying() {
-	expression += document.getElementById("display").value; 
+	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
+		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
+		s[s.length - 1] === "")
+	{
+		return;	
+	}
+
 	expression += " "
 	expression += document.getElementById("times").value
-	window.alert(expression)
+	expression += " "
 }
 
 
 function dividing() {
-	expression += document.getElementById("display").value; 
+	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
+		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
+		s[s.length - 1] === "")
+	{
+		return;	
+	}
+
 	expression += " "
 	expression += document.getElementById("divide").value
-	window.alert(expression)
+	expression += " "
 }
 
 function clearing(){
-
+	document.getElementById("display").value = "";
+	expression = "";
 }
 
 function equaling(){
+	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
+		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
+		s[s.length - 1] === "")
+	{
+		window.alert("Invalid calculation.");
+		return;	
+	}
+
+	document.getElementById("display").value = eval(expression);	
 
 }
 
 
-function test() {
-	window.alert(document.getElementById("display").value)
+function displayCheck(numID) {
+	var s = expression.trim().split(" ");
 
+	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
+		s[s.length - 1] === "*" || s[s.length - 1] === "/")
+	{
+		document.getElementById("display").value = numID;
+	}
+
+	expression += numID;
 }
-
