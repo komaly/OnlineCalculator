f6f29a1e0dd2d1dd821f209db4acf5d9a36efbed
komaly <kyaseen@uci.edu>
2018-10-17 14:51:14 -0700

    changes

--- app/controllers/calculate_controller.rb
+++
@@ -1,4 +0,0 @@
-class CalculateController < ApplicationController
-  def index
-  end
-end
--- test/controllers/calculate_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CalculateControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get calculate_index_url
-    assert_response :success
-  end
-
-end
