9551e7f1366f0a02f02ebc0f24c65bbb75f35550
komaly <kyaseen@uci.edu>
2018-10-17 09:55:25 -0700

    Calculator

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -4,6 +4,9 @@ var toAdd = ""
 function adding() {
 	var s = expression.split(" ")
 
+	if (s[s.length - 1] === "NEW")
+		expression = expression.replace('NEW', '');
+
 	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
 		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
 		s[s.length - 1] === "")
@@ -14,10 +17,15 @@ function adding() {
 	expression += " "
 	expression += document.getElementById("plus").value
 	expression += " "
+
+	document.getElementById("equation").innerHTML = expression;
 }
 
 function subtracting() {
 	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "NEW")
+		expression = expression.replace('NEW', '');
 
 	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
 		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
@@ -29,10 +37,15 @@ function subtracting() {
 	expression += " "
 	expression += document.getElementById("minus").value
 	expression += " "
+
+	document.getElementById("equation").innerHTML = expression;
 }
 
 function multiplying() {
 	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "NEW")
+		expression = expression.replace('NEW', '');
 
 	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
 		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
@@ -44,11 +57,16 @@ function multiplying() {
 	expression += " "
 	expression += document.getElementById("times").value
 	expression += " "
+
+	document.getElementById("equation").innerHTML = expression;
 }
 
 
 function dividing() {
 	var s = expression.split(" ")
+
+	if (s[s.length - 1] === "NEW")
+		expression = expression.replace('NEW', '');
 
 	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
 		s[s.length - 1] === "*" || s[s.length - 1] === "/" ||
@@ -60,11 +78,14 @@ function dividing() {
 	expression += " "
 	expression += document.getElementById("divide").value
 	expression += " "
+
+	document.getElementById("equation").innerHTML = expression;
 }
 
 function clearing(){
 	document.getElementById("display").value = "";
 	expression = "";
+	document.getElementById("equation").innerHTML = "         ";
 }
 
 function equaling(){
@@ -79,12 +100,23 @@ function equaling(){
 	}
 
 	document.getElementById("display").value = eval(expression);	
-
+	document.getElementById("equation").innerHTML = document.getElementById("display").value;
+	expression = document.getElementById("display").value;
+	expression += " NEW"
 }
 
 
 function displayCheck(numID) {
 	var s = expression.trim().split(" ");
+
+	if (s[s.length - 1] === "NEW")
+	{
+		document.getElementById("display").value = numID;
+		expression = numID;
+		document.getElementById("equation").innerHTML = expression;
+		return;
+
+	}
 
 	if (s[s.length - 1] === "+" || s[s.length - 1] === "-" ||
 		s[s.length - 1] === "*" || s[s.length - 1] === "/")
@@ -93,4 +125,6 @@ function displayCheck(numID) {
 	}
 
 	expression += numID;
+
+	document.getElementById("equation").innerHTML = expression;
 }
